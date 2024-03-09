ab4c45d63ea0756337837500849bcdd668c28516
gluharina <72268593+gluharina@users.noreply.github.com>
2024-03-10 01:46:51 +0300

    Update u_solver.m

--- u_solver.m
+++ u_solver.m
@@ -1,60 +1,81 @@
-clear all;
+clearvars; %очищение переменных
 
-syms T;
+%ввод символов, которые будут использоваться для символического задания системы уравнений
+er = 0;
+while er < 1
+    str = input('введите символы: ','s'); %ввод символов одной строкой через пробел
+    C = strsplit(str); %разделение строки на массив подстрок 
+    er = input ('подтвердить ввод (1 - да, 0 - нет): '); %проверить ранне введеные символы 
+end 
 
-str = input('введите символы: ','s');
-C = strsplit(str);
+%перевод подстрок в символы
 i=0;
 while i<size(C,2)
     i = i+1;
-    syms(C(i));
+    syms(C(i)); %каждый элемент массива подстрок C переводится в sym 
 end
 
-n = input ('колво уравнений в системе ');
-
-z = zeros(n,3);
-z = sym(z);
-i=0;
-while i<n
-    i = i+1;
-    z(i,1) = str2sym(strcat('x',string(i),'(t)'));
-    z(i,2) = input (strcat('x',string(i),' = '));
-    if strfind(string(z(i,2)), 'u') > 0
-        z(i,3) = 1;
+n = input ('введите количество уравнений в системе: '); % n - количество уравнений в системе
+z = sym(zeros(n,3));% z = sym(z); %массив, в котором содержатся: 1 столбец - левая сторона уравнений, 2 столбец - правая сторона уравнений, 3 столбец - наличие в уравнении управления u
+syms T u; %?????????? символы T и u
+er = 0;
+while er < 1
+    disp('введите уравнения (управление u должно быть указано)');
+    i=0; u_counter = 0;
+    while i <= n
+        i = i+1; 
+        if i < n + 1
+            z(i,1) = str2sym(strcat('x',string(i),'(t)')); %заполняется первый столбец массива z - левая сторона уравнения
+            z(i,2) = input (strcat('x',string(i),' = ')); %заполняется второй столбец массива z - правая сторона уравнения
+            if strfind(string(z(i,2)), 'u') > 0
+                z(i,3) = 1; %заполняется третий столбец массива z - наличие в уравнении управления u
+                u_counter = u_counter + 1;
+            end
+        end
+        if u_counter < 1 && i == n %проверка на наличие u в системе уравнений
+            disp('управление не было введено');
+            i = 0;
+        end
     end
+    er = input ('подтвердить ввод (1 - да, 0 - нет): ');
 end
 
+er = 0;
+while er < 1
+    psi = input ('цель = '); % psi - цель
+    er = input ('подтвердить ввод (1 - да, 0 - нет): ');
+end
 
-psi = input ('цель = ');
-psi_z = zeros(n,1);
-i=0;k = 0;
-while i<n
+i = 0; k = 0; %счетчик k = 1 - цель задана по x_i по которому введено управление, k = 0 - цель и управление заданы по разным x_i
+while i < n
     i = i+1;
-    if strfind(string(psi), string(z(i,1))) > 0 
-        if z(i,3) > 0
-            k = k + 1;
+    if strfind(string(psi), string(z(i,1))) > 0 % если в цели присутвует x_i  
+        if z(i,3) > 0 %если в управление в системе по x_i
+            k = k + 1; 
         end
     end
 end 
 
-i=0;
+%вспомогательные массивы для замен
 old = sym(zeros(1,2));
 new = sym(zeros(1,2));
+i = 0;
 while i<n
     i = i+1;
-    old(i) = diff(z(i,1));
-    new(i) = z(i,2);
+    old(i) = diff(z(i,1)); %массив заполняется симовольными значениями типа diff(x_i(t), t)
+    new(i) = z(i,2); %массив заполняется правой частью x_i(t)
 end
 
-syms T;
-if k > 0 
-    sol = subs(diff(psi), old, new);
-    EL = simplify(T*sol + psi);
+if k > 0 %если счетчик k = 1, то цель задана по x_i по которому введено управление
+    sol = subs(diff(psi), old, new); %замена в diff(psi) - diff(x_i(t), t) на правую часть x_i(t)
+    EL = simplify(T*sol + psi); %уравнение эйдера-лагранжа
+    disp('-------------------->'); %вывод управления
+    disp('u = ');
     pretty(solve(EL,u))
-else 
+else %если счетчик k = 0 - цель и управление заданы по разным x_i
     syms fi T1 T2;
     i=0;
-    psi_1 = psi-psi;
+    psi_1 = sym('0');
     while i<n
         i = i+1;
         if strfind(string(psi), string(z(i,1))) > 0 
@@ -84,5 +105,3 @@ else
     disp('diff_fi = ');
     pretty(diff_fi)
 end
-
-
